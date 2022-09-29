31683c77b3cb4d27ee240c5224607afb81cfce78
Nguyen Thanh Luan <luann4099@gmail.com>
2022-09-29 20:02:30 +0700

    update session 2

---
+++ multiple_arr.php
@@ -0,0 +1,36 @@
+<?php
+$arr = [
+    "user1" => [
+        "name" => "Nam",
+        "address" => "Ha Noi"
+    ],
+    "user2" => [
+        "name" => "Linh",
+        "address" => "Hai Phong"
+    ]
+];
+
+echo $arr["user2"]["address"];
+foreach ($arr as $kUser=>$vUser) {
+    foreach ($vUser as $k => $value) {
+        echo $value;
+    }
+}
+
+$arr2 = [
+    [1, 2, 3, 3, 23, 223],
+    [121, 32434, 54545,
+        [
+            1212, 12, 12, 34434]
+    ]
+];
+echo $arr2[1][3][3]//34434
+
+$arr3 = [
+    [1, 2, 23, 232],
+    [
+        "user" => [
+            "address" => "Ha Noi"
+        ]
+    ]
+];
